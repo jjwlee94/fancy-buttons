276cdf673e4cad565e03557a3005d4ddd49daac8
Jeewon Lee <jeewon.jlee@gmail.com>
2021-12-06 10:17:29 -0500

    Create handleClick function for all buttons

---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react"; //optional
+
+function LightSwitchButton(props) {
+  const [light, setLight] = useState("off");
+
+  const handleClick = () => setLight(light === "on" ? "off" : "on");
+
+  return (
+    <button onClick={handleClick} className="LightSwitchButton">
+      {light === "on" && (
+        <span className="on">
+          <i>💡</i> I'm on!
+        </span>
+      )}
+      {light === "off" && (
+        <span className="off">
+          <i>💡</i> I'm off!
+        </span>
+      )}
+    </button>
+  );
+}
+
+export default LightSwitchButton;
